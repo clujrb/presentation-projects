d2d081020914680eeac6c7cd34383b4bf4aaf5fc
Istvan Hoka <istvan.hoka@gmail.com>
2010-08-12 12:05:24 +0300

    Added jquery_rails23 project

---
+++ 2010-08-12/jquery_rails23/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '3906865add2963f1293e2902c71c888072adbf0256051a8bbde63a57d58ee6e9f183a2c764ef6b996bc47e2467482bf7ddcd1f61b5c906fa6c84de081535691c';
