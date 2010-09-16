17a0f224d264b6bf0215737c8d22894f95e1965c
Istvan Hoka <istvan.hoka@gmail.com>
2010-09-16 13:17:27 +0300

    Adaugat 2010-09-16/jasmine_bdd.

---
+++ 2010-09-16/jasmine_bdd/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineBdd::Application.config.secret_token = '84832a2b0fd354676001f7b25aa3ea2f9510e0301375b5a79041be294885188ef67cf63e57833422ba4610ce94400091b93ae2b83fb3bbca6f115973b87d4885'
